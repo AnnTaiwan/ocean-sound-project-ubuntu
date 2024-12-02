f97947b9552271c0bba5174ab161d361cee8a069
Ann <ann20021007@gmail.com>
2024-12-02 20:13:00 +0800

    Only output rgb array

--- plot_mel_spec/on_ubuntu/kv260_build_mel_spec.sh
+++ plot_mel_spec/on_ubuntu/kv260_build_mel_spec.sh
@@ -4,3 +4,5 @@ CAIRO_FLAGS=$(pkg-config --cflags --libs cairo)
 
 $CXX -std=c++17 -O2 -o cal_mel_spec_ver4_for_ocean cal_mel_spec_ver4_for_ocean.cpp ${GSTREAMER_FLAGS}
 $CXX -std=c++17 -O2 -o plot_mel_spec_from_txt_ver2_for_ocean plot_mel_spec_from_txt_ver2_for_ocean.cpp ${CAIRO_FLAGS}
+
+$CXX -std=c++17 -O2 -o plot_mel_spec_from_txt_out_RGB_for_ocean plot_mel_spec_from_txt_out_RGB_for_ocean.cpp 
